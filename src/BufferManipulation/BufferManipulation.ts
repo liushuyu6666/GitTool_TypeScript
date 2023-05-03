557d469b7e6b659b045ad9f54025f6c167338cd5
shuyu liu <liushuyu6666@gmail.com>
2023-05-03 11:29:28 -0400

    add buffershift dependency and BufferManipulation class, unit test for BufferManipulation

---
+++ src/BufferManipulation/BufferManipulation.ts
@@ -0,0 +1,161 @@
+const buffershift = require('buffershift');
+
+export const BYTE_POWER = Math.pow(2, 8);
+
+/**
+ * This is for extract variable length integer from bytes.
+ * 
+ * If there are two binary variable, variable_a = 10100 and variable_b = 1100011.
+ * 
+ * For BE (isBE is true): The former variable will be more significant. So, variable_a will be
+ * put in front of variable_b. The answer becomes 0000(1010, 01100011).
+ * 
+ * 
+ * For LE (isBE is false): The latter variable will be more significant. So, variable_b will be
+ * put in front of variable_a, The answer becomes 0000(1100, 01110100).
+ */
+export interface BufferManipulationInterface {
+  stock: Buffer;
+
+  /*
+    If isBE = true: shuttle stores from left to right.
+    If isBE = false: shuttle stores from right to left.
+  */
+  shuttle: number;
+
+  capacityOfShuttle: number;
+
+  /*
+    true: For byte1, byte2, byte3, it stores as (Big) byte1 byte2 byte3.
+    false: For byte1, byte2, byte3, it stores as (Big) byte3 byte2 byte1.
+  */
+  isBE: boolean;
+
+  cleanTheShuttleTail(): void;
+
+  assertShuttleIsByte(): void;
+
+  fill(value: number, startIndex: number, endIndex: number): void;
+
+  finish(): void;
+
+  clean(): void;
+
+  readIntBE(): number;
+}
+
+// TODO: need setter and getter
+export class BufferManipulation implements BufferManipulationInterface {
+  stock: Buffer;
+
+  shuttle: number;
+
+  capacityOfShuttle: number;
+
+  isBE: boolean;
+
+  constructor(isBE: boolean) {
+    this.stock = Buffer.alloc(0);
+
+    this.shuttle = 0x00; // one byte
+
+    this.capacityOfShuttle = 8;
+
+    this.isBE = isBE;
+  }
+
+  cleanTheShuttleTail() {
+    // precedent:
+    // >> or <<
+    // & or |
+    this.shuttle = this.isBE
+      ? (((this.shuttle >> this.capacityOfShuttle) & 0x0ff) <<
+          this.capacityOfShuttle) &
+        0xff
+      : (((this.shuttle << this.capacityOfShuttle) & 0x0ff) >>
+          this.capacityOfShuttle) &
+        0xff;
+  }
+
+  assertShuttleIsByte() {
+    if (this.shuttle > 0xff)
+      throw new Error(
+        '[ManipulateBuffer error]: shuttle contains more that one byte',
+      );
+  }
+
+  // start from 0 and end at 8.
+  // startIndex is inclusive and endIndex is exclusive.
+  fill(value: number, startIndex: number, endIndex: number) {
+    if (startIndex > endIndex || startIndex < 0 || endIndex > 8) {
+      throw new Error(
+        `[ManipulateBuffer error]: startIndex or endIndex is illegal`,
+      );
+    }
+
+    const validSize = endIndex - startIndex;
+
+    // clear value
+    value = this.isBE
+      ? // value should be left alignment, like xxxxx000 (x is the valid bit).
+        (((value >> (8 - endIndex)) & 0xff) << (8 - endIndex + startIndex)) &
+        0xff
+      : // value should be right alignment, like 000xxxxx (x is the valid bit).
+        (((value << startIndex) & 0xff) >> (8 - endIndex + startIndex)) & 0xff;
+    value = value & 0xff; // ensure only lowest 8 bits are left.
+
+    // bits left after loading
+    const toBeLeft = this.isBE
+      ? (value << this.capacityOfShuttle) & 0xff // toBeLeft should be left alignment, like xxx00000 (x is the valid bit).
+      : (value >> this.capacityOfShuttle) & 0xff; // toBeLeft should be right alignment, like 00000xxx (x is the valid bit).
+    const newValidSize = Math.max(0, validSize - this.capacityOfShuttle); // no bit left if it is 0
+
+    this.cleanTheShuttleTail();
+
+    // load value to the shuttle
+    const toBeLoaded =
+      (this.isBE
+        ? value >> (8 - this.capacityOfShuttle)
+        : value << (8 - this.capacityOfShuttle)) & 0xff;
+    this.shuttle = this.shuttle | toBeLoaded;
+    this.capacityOfShuttle = Math.max(0, this.capacityOfShuttle - validSize); // new capacity size
+
+    this.cleanTheShuttleTail();
+
+    this.assertShuttleIsByte();
+
+    // append shuttle to the stock if necessary and build new shuttle
+    if (this.capacityOfShuttle === 0) {
+      const uint8 = new Uint8Array([this.shuttle]);
+      this.stock = this.isBE
+        ? Buffer.concat([this.stock, uint8], this.stock.length + uint8.length) // stock grows from left to right.
+        : Buffer.concat([uint8, this.stock], this.stock.length + uint8.length); // stock grows from right to left.
+      this.shuttle = toBeLeft;
+      this.capacityOfShuttle = 8 - newValidSize;
+    }
+  }
+
+  clean() {
+    this.stock = Buffer.alloc(0);
+    this.shuttle = 0b00000000;
+    this.capacityOfShuttle = 8;
+  }
+
+  finish() {
+    const rightLeft = this.capacityOfShuttle;
+    this.fill(0b00000000, 0, this.capacityOfShuttle);
+
+    if (this.isBE) {
+      buffershift.shr(this.stock, rightLeft);
+    }
+  }
+
+  readIntBE(): number {
+    let final: number = 0;
+    for (const [_, value] of this.stock.entries()) {
+      final = final * BYTE_POWER + value;
+    }
+    return final;
+  }
+}
+
---
+++ test/BufferManipulation/BufferManipulation.test.ts
@@ -0,0 +1,199 @@
+import { BufferManipulation } from "../../src/BufferManipulation/BufferManipulation";
+
+describe("Test BufferManipulation class", () => {
+    let bufferManipulation: BufferManipulation;
+
+    describe("in the BE mode (read from left to right):", () => {
+        beforeEach(() => {
+            bufferManipulation = new BufferManipulation(true);
+        });
+
+        test('The cleanTheShuttleTail method should wipe out 2 rightmost bits and return 0b11001100.', () => {
+            bufferManipulation.shuttle = 0b11001111; // TODO: need setter and getter
+            bufferManipulation.capacityOfShuttle = 2;
+        
+            bufferManipulation.cleanTheShuttleTail();
+        
+            expect(bufferManipulation.shuttle).toBe(0b11001100);
+        });
+      
+        test('The fill method should leave all valid bits in the shuttle when the shuttle is not full.', () => {
+            bufferManipulation.fill(0b11011111, 2, 5);
+        
+            expect(bufferManipulation.shuttle).toBe(0b01100000); // left alignment
+            expect(bufferManipulation.capacityOfShuttle).toBe(5);
+        });
+
+        test('The fill method should leave the shuttle empty and load all valid bits to the stock when there are exactly 8 valid bits in the shuttle.', () => {
+            bufferManipulation.fill(0b00001111, 0, 8);
+        
+            expect(bufferManipulation.shuttle).toBe(0b00000000);
+            expect(bufferManipulation.capacityOfShuttle).toBe(8);
+            expect(bufferManipulation.stock).toEqual(Buffer.from([0b00001111]));
+        });
+    
+        test('The fill method should leave all valid bits in the shuttle when the total valid bits of multiple input variables is less than 8.', () => {
+            bufferManipulation.fill(0b01001111, 1, 6); // x10011xx
+            bufferManipulation.fill(0b10101111, 2, 4); // xx10xxxx
+        
+            expect(bufferManipulation.shuttle).toBe(0b10011100); // left alignment
+            expect(bufferManipulation.capacityOfShuttle).toBe(1);
+        });
+    
+        test('The fill method should load 8 leftmost bits to the stock when the total valid bits of multiple input variables is larger than 8.', () => {
+            bufferManipulation.fill(0b01001111, 1, 6); // x10011xx
+            bufferManipulation.fill(0b10101111, 2, 6); // xx1011xx
+        
+            expect(bufferManipulation.shuttle).toBe(0b10000000);
+            expect(bufferManipulation.capacityOfShuttle).toBe(7);
+            expect(bufferManipulation.stock).toEqual(Buffer.from([0b10011101]));
+        });
+    
+        test('The fill method should work well when there are multiple input variables.', () => {
+            bufferManipulation.fill(0b10100111, 3, 8); // 0bxxx00111 new stock: null    , shuttle: 00111xxx.
+            bufferManipulation.fill(0b01010101, 1, 8); // 0bx1010101 new stock: 00111101, shuttle: 0101xxxx.
+            bufferManipulation.fill(0b10010100, 1, 7); // 0bx001010x new stock: 01010010, shuttle: 10xxxxxx.
+            bufferManipulation.fill(0b11111101, 0, 8); // 0b11111101 new stock: 10111111, shuttle: 01xxxxxx.
+            bufferManipulation.fill(0b01010101, 3, 5); // 0bxxx10xxx new stock: null    , shuttle: 0110xxxx.
+        
+            // [00111101, 01010010, 10111111]
+        
+            expect(bufferManipulation.shuttle).toBe(0b01100000); // left alignment
+            expect(bufferManipulation.capacityOfShuttle).toBe(4);
+            expect(bufferManipulation.stock).toEqual(
+                Buffer.from([0b00111101, 0b01010010, 0b10111111]),
+            );
+        });
+    
+        test('The finish method should load all left valid bits in the shuttle to the stock.', () => {
+            bufferManipulation.fill(0b10100111, 3, 8); // 0bxxx00111
+            bufferManipulation.fill(0b01010101, 1, 8); // 0bx1010101
+            bufferManipulation.fill(0b10010100, 1, 7); // 0bx001010x
+            bufferManipulation.fill(0b11111101, 0, 8); // 0b11111101
+            bufferManipulation.fill(0b01010101, 3, 5); // 0bxxx10xxx
+            bufferManipulation.finish();
+        
+            // [00000011, 11010101, 00101011,11110110]
+        
+            expect(bufferManipulation.shuttle).toBe(0b00000000); // left alignment
+            expect(bufferManipulation.capacityOfShuttle).toBe(8);
+            expect(bufferManipulation.stock).toEqual(
+                Buffer.from([0b00000011, 0b11010101, 0b00101011, 0b11110110]),
+            );
+        });
+    
+        test('The readIntBE should read from the stock.', () => {
+            bufferManipulation.fill(0b10100111, 3, 8); // 0bxxx00111
+            bufferManipulation.fill(0b01010101, 1, 8); // 0bx1010101
+            bufferManipulation.fill(0b10010100, 1, 7); // 0bx001010x
+            bufferManipulation.fill(0b11111101, 0, 8); // 0b11111101
+            bufferManipulation.fill(0b01010101, 3, 5); // 0bxxx10xxx
+            bufferManipulation.finish();
+        
+            expect(bufferManipulation.readIntBE()).toBe(
+                Buffer.from(
+                    [
+                        0b00000011,
+                        0b11010101, 
+                        0b00101011, 
+                        0b11110110,
+                    ]).readInt32BE(),
+            ); // left alignment
+        });
+    });
+
+    describe('in the LE mode (read from right to left):', () => {
+        beforeEach(() => {
+            bufferManipulation = new BufferManipulation(false);
+        });
+    
+        test('The cleanTheShuttleTail method should wipe out 2 leftmost bits and return 0b00001111.', () => {
+            bufferManipulation.shuttle = 0b11001111; // TODO: need setter and getter
+            bufferManipulation.capacityOfShuttle = 2;
+        
+            bufferManipulation.cleanTheShuttleTail();
+        
+            expect(bufferManipulation.shuttle).toBe(0b00001111);
+        });
+    
+        test('The fill method should leave all valid bits in the shuttle when the shuttle is not full.', () => {
+            bufferManipulation.fill(0b11011111, 2, 5); // 0bxx011xxx
+        
+            expect(bufferManipulation.shuttle).toBe(0b00000011); // right alignment
+            expect(bufferManipulation.capacityOfShuttle).toBe(5);
+        });
+    
+        test('The fill method should leave the shuttle empty and load all valid bits to the stock when there are exactly 8 valid bits in the shuttle.', () => {
+            bufferManipulation.fill(0b00001111, 0, 8);
+        
+            expect(bufferManipulation.shuttle).toBe(0b00000000);
+            expect(bufferManipulation.capacityOfShuttle).toBe(8);
+            expect(bufferManipulation.stock).toEqual(Buffer.from([0b00001111]));
+        });
+    
+        test('The fill method should leave all valid bits in the shuttle when the total valid bits of multiple input variables is less than 8.', () => {
+            bufferManipulation.fill(0b01001111, 1, 6); // x10011xx
+            bufferManipulation.fill(0b10101111, 2, 4); // xx10xxxx
+        
+            expect(bufferManipulation.shuttle).toBe(0b01010011); // right alignment
+            expect(bufferManipulation.capacityOfShuttle).toBe(1);
+        });
+    
+        test('The fill method should load 8 rightmost bits to the stock when the total valid bits of multiple input variables is larger than 8.', () => {
+            bufferManipulation.fill(0b01001111, 1, 6); // x10011xx
+            bufferManipulation.fill(0b10101111, 2, 6); // xx1011xx
+        
+            expect(bufferManipulation.shuttle).toBe(0b00000001);
+            expect(bufferManipulation.capacityOfShuttle).toBe(7);
+            expect(bufferManipulation.stock).toEqual(Buffer.from([0b01110011]));
+        });
+    
+        test('The fill method should work well when there are multiple input variables.', () => {
+            bufferManipulation.fill(0b10100111, 3, 8); // 0bxxx00111
+            bufferManipulation.fill(0b01010101, 1, 8); // 0bx1010101
+            bufferManipulation.fill(0b10010100, 1, 7); // 0bx001010x
+            bufferManipulation.fill(0b11111101, 0, 8); // 0b11111101
+            bufferManipulation.fill(0b01010101, 3, 5); // 0bxxx10xxx
+        
+            expect(bufferManipulation.shuttle).toBe(0b00001011); // right alignment
+            expect(bufferManipulation.capacityOfShuttle).toBe(4);
+            expect(bufferManipulation.stock).toEqual(
+                Buffer.from([0b11110100, 0b10101010, 0b10100111]), // 00001011, 11110100, 10101010, 10100111
+            );
+        });
+    
+        test('The finish method should load all left valid bits in the shuttle to the stock.', () => {
+            bufferManipulation.fill(0b10100111, 3, 8); // 0bxxx00111
+            bufferManipulation.fill(0b01010101, 1, 8); // 0bx1010101
+            bufferManipulation.fill(0b10010100, 1, 7); // 0bx001010x
+            bufferManipulation.fill(0b11111101, 0, 8); // 0b11111101
+            bufferManipulation.fill(0b01010101, 3, 5); // 0bxxx10xxx
+            bufferManipulation.finish();
+        
+            expect(bufferManipulation.shuttle).toBe(0b00000000); // right alignment
+            expect(bufferManipulation.capacityOfShuttle).toBe(8);
+            expect(bufferManipulation.stock).toEqual(
+                Buffer.from([0b00001011, 0b11110100, 0b10101010, 0b10100111]),
+            );
+        });
+    
+        test('The readIntBE should read from the stock.', () => {
+            bufferManipulation.fill(0b10100111, 3, 8); // 0bxxx00111
+            bufferManipulation.fill(0b01010101, 1, 8); // 0bx1010101
+            bufferManipulation.fill(0b10010100, 1, 7); // 0bx001010x
+            bufferManipulation.fill(0b11111101, 0, 8); // 0b11111101
+            bufferManipulation.fill(0b01010101, 3, 5); // 0bxxx10xxx
+            bufferManipulation.finish();
+        
+            expect(bufferManipulation.readIntBE()).toBe(
+                Buffer.from(
+                    [
+                        0b00001011, 
+                        0b11110100, 
+                        0b10101010, 
+                        0b10100111,
+                    ]).readInt32BE()
+            ); // right alignment
+        });
+    });
+})
