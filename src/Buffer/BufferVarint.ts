43f8e84ee998c6ddc29ff9db3bdf997ab15c27b0
shuyu liu <liushuyu6666@gmail.com>
2023-05-03 15:53:17 -0400

    update BufferManipulation to return size instead of Buffer

--- src/Buffer/BufferVarint.ts
+++ src/Buffer/BufferVarint.ts
@@ -7,7 +7,7 @@ export class BufferVarint {
         this._bufferManipulation = new BufferManipulation(true);
     }
 
-    private _concatenate(buffer: Buffer, startIdx: number): [Buffer, number] {
+    private _concatenate(buffer: Buffer, startIdx: number): [number, number] {
         let msb: boolean = buffer[0] >= 0b10000000;
         let index = 1;
         this._bufferManipulation.fill(buffer[0], startIdx, 8);
@@ -19,15 +19,15 @@ export class BufferVarint {
         }
         this._bufferManipulation.finish();
         
-        return [this._bufferManipulation.stock, index];
+        return [this._bufferManipulation.readIntBE(), index];
     }
 
-    public getFirstVarintWithType(buffer: Buffer): [[Buffer, number], number] {
+    public getFirstVarintWithType(buffer: Buffer): [[number, number], number] {
         const typeNumber = (buffer[0] & 0b01110000) >> 4 & 0b00000111;
         return [this._concatenate(buffer, 4), typeNumber];
     }
 
-    public getFirstVarintWithoutType(buffer: Buffer): [Buffer, number] {
+    public getFirstVarintWithoutType(buffer: Buffer): [number, number] {
         return this._concatenate(buffer, 1);
     }
 }
--- test/Buffer/BufferVarint.test.ts
+++ test/Buffer/BufferVarint.test.ts
@@ -9,18 +9,15 @@ describe("Test BufferVarint class", () => {
         });
 
         test("The getFirstVarintWithType function should return the right type and a 4-bit variable-length integer with the end index for a 1-byte buffer.", () => {
-            const expBuf = Buffer.from([0b00111101]); // type: 011; data: 1101
-            const [[actualBuf, idx], type] = bufferVarint.getFirstVarintWithType(expBuf);
-            expect(actualBuf).toEqual(Buffer.from([0b00001101]));
+            const buf = Buffer.from([0b00111101]); // type: 011; data: 1101
+            const [[size, idx], type] = bufferVarint.getFirstVarintWithType(buf);
+            expect(size).toEqual(13);
             expect(idx).toEqual(1);
             expect(type).toEqual(3);
-
-            // expect: 208 =          11010000
-            // receive: 6 128 =  00000110 10000000
         });
 
         test("The getFirstVarintWithType function should return the right type and a variable-length integer with the end index", () => {
-            const expBuf = Buffer.from(
+            const buf = Buffer.from(
                 [
                     0b10111101, 
                     0b11010010, 
@@ -28,8 +25,8 @@ describe("Test BufferVarint class", () => {
                     0b00111001
                 ]
             ); // type: 011; data: 00000001 10110100 10011011 00111001
-            const [[actualBuf, idx], type] = bufferVarint.getFirstVarintWithType(expBuf);
-            expect(actualBuf).toEqual(Buffer.from([0b00000001, 0b10110100, 0b10011011, 0b00111001]));
+            const [[size, idx], type] = bufferVarint.getFirstVarintWithType(buf);
+            expect(size).toEqual(28613433);
             expect(idx).toEqual(4);
             expect(type).toEqual(3);
         });
