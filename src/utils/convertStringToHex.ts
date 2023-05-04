0c0a0116143531e8b388d28c81ae5f52bbe4ec39
shuyu liu <liushuyu6666@gmail.com>
2023-05-04 15:34:46 -0400

    remove convertStringToHex function

--- src/utils/convertStringToHex.ts
+++
@@ -1,3 +0,0 @@
-export default function(hexString: string): Buffer {
-    return Buffer.from(hexString, "hex");
-}
--- test/utils/convertStringToHex.test.ts
+++
@@ -1,9 +0,0 @@
-import convertStringToHex from "../../src/utils/convertStringToHex";
-
-describe("Test convertStringToHex function,", () => {
-    test("it should return a byte array.", () => {
-        const hexString = '530c0df30bbf796ec9fa97c06c8fbee732e97d70';
-        const hex = convertStringToHex(hexString);
-        expect(hex.length).toEqual(20);
-    })
-})
