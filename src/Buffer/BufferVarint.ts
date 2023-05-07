2c239747fe103dd1a8afafdf177b8a41ce0ebc74
shuyu liu <liushuyu6666@gmail.com>
2023-05-07 17:53:41 -0400

    add getSizeEncoding

--- src/Buffer/BufferVarint.ts
+++ src/Buffer/BufferVarint.ts
@@ -9,7 +9,7 @@ export class BufferVarint {
         this._bufferManipulation = new BufferManipulation(true);
     }
 
-    private _concatenate(buffer: Buffer, startIdx: number): [number, number] {
+    public getSizeEncoding(buffer: Buffer, startIdx: number): [number, number] {
         let msb: boolean = buffer[0] >= 0b10000000;
         let index = 1;
         this._bufferManipulation.fill(buffer[0], startIdx, 8);
@@ -39,11 +39,11 @@ export class BufferVarint {
 
     public getFirstVarintWithType(buffer: Buffer): [[number, number], number] {
         const typeNumber = (buffer[0] & 0b01110000) >> 4 & 0b00000111;
-        return [this._concatenate(buffer, 4), typeNumber];
+        return [this.getSizeEncoding(buffer, 4), typeNumber];
     }
 
     public getOffsetEncoding(buffer: Buffer): [number, number] {
-        const [negativeBeforeMarginValue, startIndex] = this._concatenate(buffer, 1);
+        const [negativeBeforeMarginValue, startIndex] = this.getSizeEncoding(buffer, 1);
         const marginValue = this.getOffset(startIndex);
         const negative = negativeBeforeMarginValue + marginValue;
         return [negative, startIndex];
