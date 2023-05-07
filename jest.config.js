07ce9dfeff3c35565da3d7fabec5839b81b1a43a
shuyu liu <liushuyu6666@gmail.com>
2023-05-07 17:50:28 -0400

    remove test/archive from the unit test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['<rootDir>/test/**/?(*.)+(spec|test).[jt]s?(x)']
+    testMatch: [
+        '<rootDir>/test/**/?(*.)+(spec|test).[jt]s?(x)'
+    ],
+    modulePathIgnorePatterns: [
+        '<rootDir>/test/archive'
+    ]
 };
