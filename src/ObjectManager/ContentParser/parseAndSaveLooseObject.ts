cb06f16d3c48fb3118ea65ac8ff7fdc80bfabddb
shuyu liu <liushuyu6666@gmail.com>
2023-05-08 18:55:34 -0400

    rename looseParser to parseAndSaveLooseObject

---
+++ src/ObjectManager/ContentParser/parseAndSaveLooseObject.ts
@@ -0,0 +1,45 @@
+import { readFileSync, writeFileSync } from "fs";
+import { isLooseObject } from "../../utils/getGitObjectType";
+import { inflateSync } from "zlib";
+import treeParser, { GitTreeObjectFileEntry } from "./treeParser";
+import commitParser, { CommitObjectInfo } from "./commitParser";
+import { GitObjectType } from "../../Enum/GitObjectType";
+import blobParser from "./blobParser";
+import { GitObject } from "../../GitObject/GitObject";
+import path from 'path';
+
+export default function (gitObject: GitObject, outObjectDir?: string): string | GitTreeObjectFileEntry[] | CommitObjectInfo | undefined {
+    const type = gitObject.gitObjectType!;
+
+    if (!isLooseObject(type)) return;
+
+    const content = readFileSync(gitObject.filePath!);
+    const decryptedBuf = inflateSync(content);
+    const body = decryptedBuf.subarray(gitObject.startIdx!, gitObject.endIdx!);
+
+    let data: string | GitTreeObjectFileEntry[] | CommitObjectInfo | undefined;
+    switch (type) {
+        case GitObjectType.BLOB: {
+            data = blobParser(body);
+            break;
+        }
+        case GitObjectType.TREE: {
+            data = treeParser(body);
+            break;
+        }
+        case GitObjectType.COMMIT: {
+            data = commitParser(body);
+            break;
+        }
+        default: {
+            return;
+        }
+    }
+
+    if(outObjectDir) {
+        const outFile = path.join(outObjectDir, gitObject.hash);
+        writeFileSync(outFile, JSON.stringify(data));
+    }
+
+    return data;
+}
--- test/ObjectManager/ContentParser/parseAndSaveLooseObject.test.ts
+++ test/ObjectManager/ContentParser/parseAndSaveLooseObject.test.ts
@@ -1,6 +1,7 @@
 import { GitObjectType } from "../../../src/Enum/GitObjectType";
+import { GitObject } from "../../../src/GitObject/GitObject";
 import { CommitObjectInfo } from "../../../src/ObjectManager/ContentParser/commitParser";
-import looseParser from "../../../src/ObjectManager/ContentParser/looseParser";
+import parseAndSaveLooseObject from "../../../src/ObjectManager/ContentParser/parseAndSaveLooseObject";
 import { GitTreeObjectFileEntry } from "../../../src/ObjectManager/ContentParser/treeParser";
 
 describe('Test looseObjectParser,', () => {
@@ -10,8 +11,17 @@ describe('Test looseObjectParser,', () => {
         const startIdx = 9;
         const endIdx = 449;
         const type = GitObjectType.BLOB;
+        const gitObject = new GitObject(
+            '6e2e2f807d5beb52bab4d672e335495b30dcb9ee',
+            type,
+            0,
+            undefined,
+            filePath,
+            startIdx,
+            endIdx
+        );
 
-        const data = looseParser(filePath, startIdx, endIdx, type) as any as string;
+        const data = parseAndSaveLooseObject(gitObject) as any as string;
 
         expect(data.substring(0, 20)).toBe('// eslint-disable-ne');
     });
@@ -22,8 +32,17 @@ describe('Test looseObjectParser,', () => {
         const startIdx = 8;
         const endIdx = 37;
         const type = GitObjectType.TREE;
+        const gitObject = new GitObject(
+            'f36108462c88b76aa014a4a298c7def1ac5fd572',
+            type,
+            0,
+            undefined,
+            filePath,
+            startIdx,
+            endIdx
+        );
 
-        const data = looseParser(filePath, startIdx, endIdx, type) as any as GitTreeObjectFileEntry[];
+        const data = parseAndSaveLooseObject(gitObject) as any as GitTreeObjectFileEntry[];
 
         expect(data[0].hash).toBe('ea155cab231708e49c486f23fadf1eda5b7c46d4');
     });
@@ -34,8 +53,17 @@ describe('Test looseObjectParser,', () => {
         const startIdx = 11;
         const endIdx = 279;
         const type = GitObjectType.COMMIT;
+        const gitObject = new GitObject(
+            '03ad3ec20a59305503cdcef6667e50fa94edc563',
+            type,
+            0,
+            undefined,
+            filePath,
+            startIdx,
+            endIdx
+        );
 
-        const data = looseParser(filePath, startIdx, endIdx, type) as any as CommitObjectInfo;
+        const data = parseAndSaveLooseObject(gitObject) as any as CommitObjectInfo;
 
         expect(data.hash.treeHash).toBe('56d45faf43f5e5acd80836af1d00edddeb0bc8a2');
     })
