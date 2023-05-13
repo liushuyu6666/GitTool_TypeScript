fb2c67339b2962e9372f164bba2094dbd214fdd6
shuyu liu <liushuyu6666@gmail.com>
2023-05-13 15:42:36 -0400

    delete parseAndSaveLooseObject and contentParser

--- test/ObjectManager/ContentParser/contentParser.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Test the contentParser function', () => {
-    // TODO: when the prod case is ready.
-    test('for parse the blob object.', () => {
-        
-        expect(true).toBe(true);
-    })
-})
